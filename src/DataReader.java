e32dde0b07af73f43a2dd607ac95fd5d3669ca08
fuzzyhappy <student.e.wang@gmail.com>
2020-11-18 23:51:31 -0500

    fixed intellij issue with Chat.java

--- src/DataReader.java
+++ src/DataReader.java
@@ -1,2 +1,7 @@
 public class DataReader {
+    public DataReader() {
+
+    }
+
+    public static
 }
--- src/Server.java
+++ src/Server.java
@@ -1,2 +1,4 @@
+import java.util.ArrayList;
 public class Server {
+    private ArrayList<Chat> chats =
 }
