006333e1743a6c8359e2473031128b82941dfcb4
fuzzyhappy <student.e.wang@gmail.com>
2020-11-18 19:49:21 -0500

    init first classes

---
+++ src/TestForm.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class TestForm {
+    private JPanel panel1;
+    private JTextField ungaBungaDaaaaaaaTextField;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
