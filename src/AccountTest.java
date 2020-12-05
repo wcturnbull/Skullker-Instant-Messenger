f8cf91ce23b98911d70f1db3e86cc0d262482d51
neeeeel <wang4919@purdue.edu>
2020-12-04 22:18:43 -0600

    completed testcases

--- src/AccountTest.java
+++ src/AccountTest.java
@@ -1,17 +1,94 @@
+import org.junit.Assert;
 import org.junit.Test;
 import java.time.*;
 import static org.junit.Assert.*;
 import java.util.*;
+import static org.junit.Assert.*;
+import java.lang.reflect.*;
 public class AccountTest {
 
+    @Test (timeout = 1000)
+    public void testFields() {
+        Field userName;
+        Field password;
+        Field serial;
+        Field chats;
+        try {
+            userName = Account.class.getDeclaredField("userName");
+            if (Modifier.isPublic(userName.getModifiers())) {
+                Assert.fail();
+            } else if (!userName.getType().equals(String.class)) {
+                Assert.fail();
+            }
+        } catch (NoSuchFieldException e) {
+            System.out.println("The userName field is missing");
+            Assert.fail();
+        }
+        try {
+            password = Account.class.getDeclaredField("password");
+            if (Modifier.isPublic(password.getModifiers())) {
+                Assert.fail();
+            } else if (!password.getType().equals(String.class)) {
+                Assert.fail();
+            }
+        } catch (NoSuchFieldException e) {
+            System.out.println("The password field is missing");
+        }
+        try {
+            serial = Account.class.getDeclaredField("serial");
+            if (Modifier.isPublic(serial.getModifiers())) {
+                Assert.fail();
+            } else if (!serial.getType().equals(String.class)) {
+                Assert.fail();
+            }
+        } catch (NoSuchFieldException e) {
+            System.out.println("The field serial is missing");
+            Assert.fail();
+        }
+        try {
+            chats = Account.class.getDeclaredField("chats");
+            if (Modifier.isPublic(chats.getModifiers())) {
+                Assert.fail();
+            } else if (!chats.getType().equals(Vector.class)) {
+                Assert.fail();
+            }
+        } catch (NoSuchFieldException e) {
+            System.out.println("The field chats is missing");
+            Assert.fail();
+        }
+    }
     @Test
     public void getUserName() {
+        Method method;
+        try {
+            method = Account.class.getMethod("getUserName");
+            if (Modifier.isPrivate(method.getModifiers())) {
+                Assert.fail();
+            } else if (!method.getReturnType().equals(String.class)) {
+                Assert.fail();
+            }
+        } catch (NoSuchMethodException e) {
+            System.out.println("The getUserName method does not exist");
+        }
         Account acc = new Account("test", "1234");
         assertEquals("test", acc.getUserName());
     }
 
     @Test
     public void setUserName() {
+        Method method;
+        try {
+            Class[] classes = new Class[1];
+            classes[0] = String.class;
+            method = Account.class.getMethod("setUserName", classes);
+            if (Modifier.isPrivate(method.getModifiers())) {
+                Assert.fail();
+            } else if (!method.getReturnType().equals(Void.TYPE)) {
+                Assert.fail();
+            }
+        } catch (NoSuchMethodException e) {
+            System.out.println("The setUserName method does not exist");
+        }
         Account acc = new Account("test", "1234");
         acc.setUserName("test2");
         assertEquals("test2",acc.getUserName());
@@ -19,12 +96,37 @@ public class AccountTest {
 
     @Test
     public void getPassword() {
+        Method method;
+        try {
+
+            method = Account.class.getMethod("getPassword");
+            if (Modifier.isPrivate(method.getModifiers())) {
+                Assert.fail();
+            } else if (!method.getReturnType().equals(String.class)) {
+                Assert.fail();
+            }
+        } catch (NoSuchMethodException e) {
+            System.out.println("The getPassword method does not exist");
+        }
         Account acc = new Account("test", "1234");
         assertEquals("1234",  acc.getPassword());
     }
 
     @Test
     public void setPassword() {
+        Method method;
+        try {
+            Class[] classes = new Class[1];
+            classes[0] = String.class;
+            method = Account.class.getMethod("setPassword", classes);
+            if (Modifier.isPrivate(method.getModifiers())) {
+                Assert.fail();
+            } else if (!method.getReturnType().equals(Void.TYPE)) {
+                Assert.fail();
+            }
+        } catch (NoSuchMethodException e) {
+            System.out.println("The setPassword method does not exist");
+        }
         Account acc = new Account("test", "1234");
         acc.setPassword("12345");
         assertEquals("12345", acc.getPassword());
@@ -32,6 +134,19 @@ public class AccountTest {
 
     @Test
     public void copy() {
+        Method method;
+        try {
+            Class[] classes = new Class[1];
+            classes[0] = Account.class;
+            method = Account.class.getMethod("copy", classes);
+            if (Modifier.isPrivate(method.getModifiers())) {
+                Assert.fail();
+            } else if (!method.getReturnType().equals(Void.TYPE)) {
+                Assert.fail();
+            }
+        } catch (NoSuchMethodException e) {
+            System.out.println("The copy method does not exist");
+        }
         Account acc = new Account("test", "1234");
         Account copy = new Account("test1", "12345");
         assertNotEquals(acc, copy);
@@ -42,6 +157,19 @@ public class AccountTest {
 
     @Test
     public void joinChat() {
+        Method method;
+        try {
+            Class[] classes = new Class[1];
+            classes[0] = Chat.class;
+            method = Account.class.getMethod("joinChat", Chat.class);
+            if (Modifier.isPrivate(method.getModifiers())) {
+                Assert.fail();
+            } else if (!method.getReturnType().equals(Void.TYPE)) {
+                Assert.fail();
+            }
+        } catch (NoSuchMethodException e) {
+            System.out.println("The joinChat method does not exist");
+        }
         Account acc = new Account("test", "1234");
         Chat chat = new Chat(acc, "Testing");
         acc.joinChat(chat);
@@ -52,6 +180,19 @@ public class AccountTest {
 
     @Test
     public void leaveChat() {
+        Method method;
+        try {
+            Class[] classes = new Class[1];
+            classes[0] = Chat.class;
+            method = Account.class.getMethod("leaveChat", classes);
+            if (Modifier.isPrivate(method.getModifiers())) {
+                Assert.fail();
+            } else if (!method.getReturnType().equals(Void.TYPE)) {
+                Assert.fail();
+            }
+        } catch (NoSuchMethodException e) {
+            System.out.println("The leaveChat method does not exist");
+        }
         Account acc = new Account("test", "1234");
         Chat chat = new Chat(acc, "Testing");
         acc.joinChat(chat);
@@ -64,6 +205,17 @@ public class AccountTest {
 
     @Test
     public void getChats() {
+        Method method;
+        try {
+            method = Account.class.getMethod("getChats");
+            if (Modifier.isPrivate(method.getModifiers())) {
+                Assert.fail();
+            } else if (!method.getReturnType().equals(Vector.class)) {
+                Assert.fail();
+            }
+        } catch (NoSuchMethodException e) {
+            System.out.println("The getChats method does not exist");
+        }
         Account acc = new Account("test", "1234");
         Chat chat = new Chat(acc, "Testing");
         Chat chat1 = new Chat(acc, "Testing1");
@@ -81,6 +233,19 @@ public class AccountTest {
 
     @Test
     public void fetchChat() {
+        Method method;
+        try {
+            Class[] classes = new Class[1];
+            classes[0] = Chat.class;
+            method = Account.class.getMethod("fetchChat", classes);
+            if (Modifier.isPrivate(method.getModifiers())) {
+                Assert.fail();
+            } else if (!method.getReturnType().equals(Chat.class)) {
+                Assert.fail();
+            }
+        } catch (NoSuchMethodException e) {
+            System.out.println("The fetchChat method does not exist");
+        }
         Account acc = new Account("test", "1234");
         Chat chat = new Chat(acc, "Testing");
         acc.joinChat(chat);
@@ -92,6 +257,17 @@ public class AccountTest {
 
     @Test
     public void delete() {
+        Method method;
+        try {
+            method = Account.class.getMethod("delete");
+            if (Modifier.isPrivate(method.getModifiers())) {
+                Assert.fail();
+            } else if (!method.getReturnType().equals(Void.TYPE)) {
+                Assert.fail();
+            }
+        } catch (NoSuchMethodException e) {
+            System.out.println("The delete method does not exist");
+        }
         Account acc = new Account("test", "1234");
         Chat chat = new Chat(acc, "Testing");
         acc.joinChat(chat);
@@ -102,6 +278,17 @@ public class AccountTest {
 
     @Test
     public void matchesUsername() {
+        Method method;
+        try {
+            method = Account.class.getMethod("matchesUsername", Account.class);
+            if (Modifier.isPrivate(method.getModifiers())) {
+                Assert.fail();
+            } else if (!method.getReturnType().equals(boolean.class)) {
+                Assert.fail();
+            }
+        } catch (NoSuchMethodException e) {
+            System.out.println("The matchesUsername method does not exist");
+        }
         Account acc = new Account("test", "1234");
         Account account = new Account("test", "12345");
         Account fake = new Account("tEst","1234");
@@ -113,6 +300,17 @@ public class AccountTest {
 
     @Test
     public void matchesCredentials() {
+        Method method;
+        try {
+            method = Account.class.getMethod("matchesCredentials", Account.class);
+            if (Modifier.isPrivate(method.getModifiers())) {
+                Assert.fail();
+            } else if (!method.getReturnType().equals(boolean.class)) {
+                Assert.fail();
+            }
+        } catch (NoSuchMethodException e) {
+            System.out.println("The matchesCredentials method does not exist");
+        }
         Account acc = new Account("test", "1234");
         Account account = new Account("test", "12345");
         Account fake = new Account("test","1234");
@@ -122,6 +320,17 @@ public class AccountTest {
 
     @Test
     public void testEquals() {
+        Method method;
+        try {
+            method = Account.class.getMethod("equals", Object.class);
+            if (Modifier.isPrivate(method.getModifiers())) {
+                Assert.fail();
+            } else if (!method.getReturnType().equals(boolean.class)) {
+                Assert.fail();
+            }
+        } catch (NoSuchMethodException e) {
+            System.out.println("The equals method does not exist");
+        }
         Account acc = new Account("test", "1234");
         Account account = new Account("test", "1234");
         Account fake = acc;
@@ -131,6 +340,17 @@ public class AccountTest {
 
     @Test
     public void testToString() {
+        Method method;
+        try {
+            method = Account.class.getMethod("toString");
+            if (Modifier.isPrivate(method.getModifiers())) {
+                Assert.fail();
+            } else if (!method.getReturnType().equals(String.class)) {
+                Assert.fail();
+            }
+        } catch (NoSuchMethodException e) {
+            System.out.println("The toString method does not exist");
+        }
         Account acc = new Account("test", "1234");
         assertEquals("test 1234 ", acc.toString());
     }
